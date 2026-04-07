f9aad4341494217316d0860980b986dc9b39dbfe
Ubuntu <ubuntu@ip-172-31-5-89.ec2.internal>
2026-04-07 12:26:00 +0000

    Add Tea to task list

--- app.js
+++ app.js
@@ -3,22 +3,21 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-// Connect to MongoDB
+console.log('MONGO_URL is:', process.env.MONGO_URL);
+
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Define Task schema
 const Task = mongoose.model('Task', new mongoose.Schema({
   id:     Number,
   name:   String,
   status: String
 }));
 
-// GET /tasks
 app.get('/tasks', async (req, res) => {
   const tasks = await Task.find({}, { _id: 0, __v: 0 });
   res.json(tasks);
 });
 
-app.listen(3000, () => console.log('App running on port 3000'));
+app.listen(3000, () => console.log('Server running on port 3000'));
